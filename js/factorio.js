0c1699c259236b5ae6c51e49de7c0c2094323aeb
LsHallo <xlshallox@gmail.com>
2020-07-31 02:15:24 +0200

    add auto expand with javascript

--- js/factorio.js
+++ js/factorio.js
@@ -1,3 +1,24 @@
 $('.problem-head').on('click', function (e) {
     $(e.currentTarget).parent().find('.problem-body').toggleClass('hidden');
 });
+
+$(function () {
+    expandTopic(window.location.hash);
+    $(window).on('hashchange', function(){
+        expandTopic(window.location.hash);
+    });
+    $('a.list-item').on('click', function () {
+        expandTopic(window.location.hash);
+    });
+});
+
+function expandTopic(selector) {
+    let elem = $(selector);
+    if(elem.hasClass('problem')) {
+        elem.find('.problem-body').removeClass('hidden');
+        elem[0].scrollIntoView();
+    } else {
+        elem.closest('.problem').find('.problem-body').removeClass('hidden');
+        elem[0].scrollIntoView();
+    }
+}
