abe9bbcd470728bea389581089ecb0d0558c0798
LsHallo <xlshallox@gmail.com>
2020-08-05 03:32:16 +0200

    improve hash handling

--- js/factorio.js
+++ js/factorio.js
@@ -3,22 +3,29 @@ $('.problem-head').on('click', function (e) {
 });
 
 $(function () {
-    expandTopic(window.location.hash);
+    if(window.location.hash.length > 0) {
+        expandTopic(window.location.hash);
+    }
     $(window).on('hashchange', function(){
         expandTopic(window.location.hash);
     });
-    $('a.list-item').on('click', function () {
-        expandTopic(window.location.hash);
+    $('a.list-item').on('click', function (e) {
+        expandTopic(e.currentTarget.hash);
     });
 });
 
 function expandTopic(selector) {
+    console.log('Trying to expand ' + selector);
     let elem = $(selector);
     if(elem.hasClass('problem')) {
         elem.find('.problem-body').removeClass('hidden');
-        elem[0].scrollIntoView();
+        if(elem[0] !== undefined) {
+            elem[0].scrollIntoView();
+        }
     } else {
         elem.closest('.problem').find('.problem-body').removeClass('hidden');
-        elem[0].scrollIntoView();
+        if(elem[0] !== undefined) {
+            elem[0].scrollIntoView();
+        }
     }
 }
